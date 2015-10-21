5db504c5606c901582cf8c1600dd90229c3bf6c6
Lauren Kett <laurenmkett@gmail.com>
2015-10-21 15:16:48 -0400

    commenting properly

--- js/sketch.js
+++ js/sketch.js
@@ -49,40 +49,40 @@ function gotForecast(){
 
 
 //defining seasons
-/*if (n <= 3|| n==12){
-Season = Winter;
-}
-
-if (n >= 3|| n<=6){
-Season = Spring;
-}
-
-if (n >= 7|| n<=9){
-Season = Summer;
-}
-
-if (n >= 10|| n<=11){
-Season = Fall;
-}
+//if (n <= 3|| n==12){
+//Season = Winter;
+//}
+//
+//if (n >= 3|| n<=6){
+//Season = Spring;
+//}
+//
+//if (n >= 7|| n<=9){
+//Season = Summer;
+//}
+//
+//if (n >= 10|| n<=11){
+//Season = Fall;
+//}
 
 
 
 //five day forecast
-object 1,3,7 ( for timing to pull from 3 hour api forecast offers)
+//object 1,3,7 ( for timing to pull from 3 hour api forecast offers)
+//
+//
+//var weekday = new Array(7);
+//weekday[0]=  "Sunday";
+//weekday[1] = "Monday";
+//weekday[2] = "Tuesday";
+//weekday[3] = "Wednesday";
+//weekday[4] = "Thursday";
+//weekday[5] = "Friday";
+//weekday[6] = "Saturday";
+//
+//var dayOfTheWeek = weekday[d.getDay()];
 
 
-var weekday = new Array(7);
-weekday[0]=  "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
-
-var dayOfTheWeek = weekday[d.getDay()];
-/*
 
 
-
-match photos with wine pairings in a document, then pair them with a randomized function to call it
+//match photos with wine pairings in a document, then pair them with a randomized function to call it
